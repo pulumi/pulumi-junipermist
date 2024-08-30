d58436d68d608788639c84a704f84dff63cae000
zbuchheit <zbuchheit@pulumi.com>
2024-08-30 08:24:08 -0700

    Add org-wlan-isolation-test
    org-wlan-isolation-test serves as a regression test for
    https://github.com/pulumi/pulumi-junipermist/issues/49

--- examples/examples_ts_test.go
+++ examples/examples_ts_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/assertpreview"
 	"github.com/pulumi/providertest/pulumitest/opttest"
 )
 
@@ -18,6 +19,21 @@ func TestOrgWlanTs(t *testing.T) {
 		opttest.LocalProviderPath("pulumi-junipermist", filepath.Join(getCwd(t), "..", "bin")),
 		opttest.YarnLink("@pulumi/juniper-mist"),
 	)
-	test.SetConfig("organizationId", os.Getenv(EnvMistOrgID))	
+	test.SetConfig("organizationId", os.Getenv(EnvMistOrgID))
 	test.Up()
+}
+func TestOrgWlanIsolationTs(t *testing.T) {
+	// Regression test for https://github.com/pulumi/pulumi-junipermist/issues/49
+	checkBaseEnvVars(t)
+	test := pulumitest.NewPulumiTest(t, "org-wlan-isolation-ts",
+		opttest.LocalProviderPath("pulumi-junipermist", filepath.Join(getCwd(t), "..", "bin")),
+		opttest.YarnLink("@pulumi/juniper-mist"),
+	)
+	test.SetConfig("organizationId", os.Getenv(EnvMistOrgID))
+	test.Up()
+	preview, error := test.CurrentStack().Preview(test.Context())
+	if error != nil {
+		t.Fatalf("failed to preview: %v", error)
+	}
+	assertpreview.HasNoChanges(t, preview)
 }
