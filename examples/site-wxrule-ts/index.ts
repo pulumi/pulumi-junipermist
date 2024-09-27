bb589fa53b9340370af7e1ea3c74243fe3681995
Zach Buchheit <zbuchheit@pulumi.com>
2024-09-27 11:17:13 -0700

    Add Wxtag and reference to site-wxrule-ts test

--- examples/site-wxrule-ts/index.ts
+++ examples/site-wxrule-ts/index.ts
@@ -8,11 +8,29 @@ const sites = junipermist.getSitesOutput({
   orgId: organizationId,
 });
 
+const wtag = new junipermist.site.Wxtag("wtag_one", {
+  siteId: sites.sites[0].id,
+  name: "wtag_one",
+  values: ["192.0.2.0/16"],
+  op: "in",
+  type: "match",
+  match: "ip_range_subnet",
+});
+
 new junipermist.site.Wxrule("pulumi-wxrule", {
   enabled: true,
   action: "allow",
-  order: 2,
+  order: -1,
   siteId: sites.sites[0].id,
   srcWxtags: ["IOT"],
   dstAllowWxtags: ["RFC-1918"],
+});
+
+new junipermist.site.Wxrule("pulumi-wxrule2", {
+  enabled: true,
+  action: "allow",
+  order: 2,
+  siteId: wtag.siteId,
+  srcWxtags: ["IOT"],
+  dstAllowWxtags: [wtag.id],
 });
