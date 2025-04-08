e491d8b6fc453afe441905badd6608b5b4066292
Venelin <venelin@pulumi.com>
2025-04-08 21:31:32 +0300

    make build_sdks

--- sdk/java/src/main/java/com/pulumi/junipermist/device/DeviceFunctions.java
+++ sdk/java/src/main/java/com/pulumi/junipermist/device/DeviceFunctions.java
@@ -7,6 +7,7 @@ import com.pulumi.core.Output;
 import com.pulumi.core.TypeShape;
 import com.pulumi.deployment.Deployment;
 import com.pulumi.deployment.InvokeOptions;
+import com.pulumi.deployment.InvokeOutputOptions;
 import com.pulumi.junipermist.Utilities;
 import com.pulumi.junipermist.device.inputs.GetApStatsArgs;
 import com.pulumi.junipermist.device.inputs.GetApStatsPlainArgs;
@@ -212,6 +213,54 @@ public final class DeviceFunctions {
      * &lt;!--End PulumiCodeChooser --&gt;
      * 
      */
+    public static Output<GetApStatsResult> getApStats(GetApStatsArgs args, InvokeOutputOptions options) {
+        return Deployment.getInstance().invoke("junipermist:device/getApStats:getApStats", TypeShape.of(GetApStatsResult.class), args, Utilities.withVersion(options));
+    }
+    /**
+     * This data source provides the list of Wireless Access Points with their statistics.
+     * 
+     * ## Example Usage
+     * 
+     * &lt;!--Start PulumiCodeChooser --&gt;
+     * <pre>
+     * {@code
+     * package generated_program;
+     * 
+     * import com.pulumi.Context;
+     * import com.pulumi.Pulumi;
+     * import com.pulumi.core.Output;
+     * import com.pulumi.junipermist.device.DeviceFunctions;
+     * import com.pulumi.junipermist.device.inputs.GetApStatsArgs;
+     * import java.util.List;
+     * import java.util.ArrayList;
+     * import java.util.Map;
+     * import java.io.File;
+     * import java.nio.file.Files;
+     * import java.nio.file.Paths;
+     * 
+     * public class App {
+     *     public static void main(String[] args) {
+     *         Pulumi.run(App::stack);
+     *     }
+     * 
+     *     public static void stack(Context ctx) {
+     *         final var apStats = DeviceFunctions.getApStats(GetApStatsArgs.builder()
+     *             .orgId("15fca2ac-b1a6-47cc-9953-cc6906281550")
+     *             .mac("5c5b35000000")
+     *             .siteId("4a422ae5-7ca0-4599-87a3-8e49aa63685f")
+     *             .status("connected")
+     *             .duration("1d")
+     *             .start(1736031600)
+     *             .end(1736175934)
+     *             .build());
+     * 
+     *     }
+     * }
+     * }
+     * </pre>
+     * &lt;!--End PulumiCodeChooser --&gt;
+     * 
+     */
     public static CompletableFuture<GetApStatsResult> getApStatsPlain(GetApStatsPlainArgs args, InvokeOptions options) {
         return Deployment.getInstance().invokeAsync("junipermist:device/getApStats:getApStats", TypeShape.of(GetApStatsResult.class), args, Utilities.withVersion(options));
     }
@@ -357,6 +406,54 @@ public final class DeviceFunctions {
      * 
      */
     public static Output<GetGatewayStatsResult> getGatewayStats(GetGatewayStatsArgs args, InvokeOptions options) {
+        return Deployment.getInstance().invoke("junipermist:device/getGatewayStats:getGatewayStats", TypeShape.of(GetGatewayStatsResult.class), args, Utilities.withVersion(options));
+    }
+    /**
+     * This data source provides the list of Gateways with their statistics.
+     * 
+     * ## Example Usage
+     * 
+     * &lt;!--Start PulumiCodeChooser --&gt;
+     * <pre>
+     * {@code
+     * package generated_program;
+     * 
+     * import com.pulumi.Context;
+     * import com.pulumi.Pulumi;
+     * import com.pulumi.core.Output;
+     * import com.pulumi.junipermist.device.DeviceFunctions;
+     * import com.pulumi.junipermist.device.inputs.GetGatewayStatsArgs;
+     * import java.util.List;
+     * import java.util.ArrayList;
+     * import java.util.Map;
+     * import java.io.File;
+     * import java.nio.file.Files;
+     * import java.nio.file.Paths;
+     * 
+     * public class App {
+     *     public static void main(String[] args) {
+     *         Pulumi.run(App::stack);
+     *     }
+     * 
+     *     public static void stack(Context ctx) {
+     *         final var gatewayStats = DeviceFunctions.getGatewayStats(GetGatewayStatsArgs.builder()
+     *             .orgId("15fca2ac-b1a6-47cc-9953-cc6906281550")
+     *             .mac("e8a245000000")
+     *             .siteId("4a422ae5-7ca0-4599-87a3-8e49aa63685f")
+     *             .status("connected")
+     *             .duration("1d")
+     *             .start(1736031600)
+     *             .end(1736175934)
+     *             .build());
+     * 
+     *     }
+     * }
+     * }
+     * </pre>
+     * &lt;!--End PulumiCodeChooser --&gt;
+     * 
+     */
+    public static Output<GetGatewayStatsResult> getGatewayStats(GetGatewayStatsArgs args, InvokeOutputOptions options) {
         return Deployment.getInstance().invoke("junipermist:device/getGatewayStats:getGatewayStats", TypeShape.of(GetGatewayStatsResult.class), args, Utilities.withVersion(options));
     }
     /**
@@ -604,6 +701,56 @@ public final class DeviceFunctions {
      * &lt;!--End PulumiCodeChooser --&gt;
      * 
      */
+    public static Output<GetSwitchStatsResult> getSwitchStats(GetSwitchStatsArgs args, InvokeOutputOptions options) {
+        return Deployment.getInstance().invoke("junipermist:device/getSwitchStats:getSwitchStats", TypeShape.of(GetSwitchStatsResult.class), args, Utilities.withVersion(options));
+    }
+    /**
+     * This data source provides the list of Switches with their statistics.
+     * 
+     * ## Example Usage
+     * 
+     * &lt;!--Start PulumiCodeChooser --&gt;
+     * <pre>
+     * {@code
+     * package generated_program;
+     * 
+     * import com.pulumi.Context;
+     * import com.pulumi.Pulumi;
+     * import com.pulumi.core.Output;
+     * import com.pulumi.junipermist.device.DeviceFunctions;
+     * import com.pulumi.junipermist.device.inputs.GetSwitchStatsArgs;
+     * import java.util.List;
+     * import java.util.ArrayList;
+     * import java.util.Map;
+     * import java.io.File;
+     * import java.nio.file.Files;
+     * import java.nio.file.Paths;
+     * 
+     * public class App {
+     *     public static void main(String[] args) {
+     *         Pulumi.run(App::stack);
+     *     }
+     * 
+     *     public static void stack(Context ctx) {
+     *         final var switchStats = DeviceFunctions.getSwitchStats(GetSwitchStatsArgs.builder()
+     *             .orgId("15fca2ac-b1a6-47cc-9953-cc6906281550")
+     *             .mac("485a0d000000")
+     *             .siteId("4a422ae5-7ca0-4599-87a3-8e49aa63685f")
+     *             .status("connected")
+     *             .evpnUnused(true)
+     *             .evpntopoId("92984e2f-94db-4cd8-9763-9cf83fbd079e")
+     *             .duration("1d")
+     *             .start(1736031600)
+     *             .end(1736175934)
+     *             .build());
+     * 
+     *     }
+     * }
+     * }
+     * </pre>
+     * &lt;!--End PulumiCodeChooser --&gt;
+     * 
+     */
     public static CompletableFuture<GetSwitchStatsResult> getSwitchStatsPlain(GetSwitchStatsPlainArgs args, InvokeOptions options) {
         return Deployment.getInstance().invokeAsync("junipermist:device/getSwitchStats:getSwitchStats", TypeShape.of(GetSwitchStatsResult.class), args, Utilities.withVersion(options));
     }
@@ -737,6 +884,50 @@ public final class DeviceFunctions {
      * 
      */
     public static Output<GetVersionsResult> getVersions(GetVersionsArgs args, InvokeOptions options) {
+        return Deployment.getInstance().invoke("junipermist:device/getVersions:getVersions", TypeShape.of(GetVersionsResult.class), args, Utilities.withVersion(options));
+    }
+    /**
+     * This data source provides the list of available Firmware Versions.
+     * 
+     * ## Example Usage
+     * 
+     * &lt;!--Start PulumiCodeChooser --&gt;
+     * <pre>
+     * {@code
+     * package generated_program;
+     * 
+     * import com.pulumi.Context;
+     * import com.pulumi.Pulumi;
+     * import com.pulumi.core.Output;
+     * import com.pulumi.junipermist.device.DeviceFunctions;
+     * import com.pulumi.junipermist.device.inputs.GetVersionsArgs;
+     * import java.util.List;
+     * import java.util.ArrayList;
+     * import java.util.Map;
+     * import java.io.File;
+     * import java.nio.file.Files;
+     * import java.nio.file.Paths;
+     * 
+     * public class App {
+     *     public static void main(String[] args) {
+     *         Pulumi.run(App::stack);
+     *     }
+     * 
+     *     public static void stack(Context ctx) {
+     *         final var ap24Versions = DeviceFunctions.getVersions(GetVersionsArgs.builder()
+     *             .orgId("15fca2ac-b1a6-47cc-9953-cc6906281550")
+     *             .type("ap")
+     *             .model("AP24")
+     *             .build());
+     * 
+     *     }
+     * }
+     * }
+     * </pre>
+     * &lt;!--End PulumiCodeChooser --&gt;
+     * 
+     */
+    public static Output<GetVersionsResult> getVersions(GetVersionsArgs args, InvokeOutputOptions options) {
         return Deployment.getInstance().invoke("junipermist:device/getVersions:getVersions", TypeShape.of(GetVersionsResult.class), args, Utilities.withVersion(options));
     }
     /**
--- sdk/java/src/main/java/com/pulumi/junipermist/org/Inventory.java
+++ sdk/java/src/main/java/com/pulumi/junipermist/org/Inventory.java
@@ -102,6 +102,7 @@ public class Inventory extends com.pulumi.resources.CustomResource {
     private static com.pulumi.resources.CustomResourceOptions makeResourceOptions(@Nullable com.pulumi.resources.CustomResourceOptions options, @Nullable Output<java.lang.String> id) {
         var defaultOptions = com.pulumi.resources.CustomResourceOptions.builder()
             .version(Utilities.getVersion())
+            .pluginDownloadURL("github://api.github.com/pulumi/pulumi-junipermist")
             .build();
         return com.pulumi.resources.CustomResourceOptions.merge(defaultOptions, options, id);
     }
